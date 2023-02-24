0d43f5fba4d9c831f532eee1337626cc58d16469
adam willetts <adam-willetts@hotmail.co.uk>
2023-02-24 19:05:19 +0000

    added all prompts and tested

--- index.mjs
+++ index.mjs
@@ -1,53 +1,71 @@
 import inquirer from "inquirer";
-import fs from "fs";
+import fs from "fs/promises";
 
-inquirer.prompt(
+const {
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contributor,
+  test,
+  github,
+  email,
+} = await inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "what is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "please write a brief description.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "how do you install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "how is your project used?",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "which of these license is applicable to your repo?",
+    choices: ["APACHE", "TOMCAST", "MIDDLEWARE", "MIT", "NONE"],
+  },
+  {
+    type: "input",
+    name: "contributor",
+    message: "please list contributors?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "require test? please provide a walkthrough for tests.",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "what is your github user name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is your email address?",
+  },
+]);
 
-    [
-        {
-            type: "input",
-            name: "title",
-            message: "what is the title of the project?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "please write a brief description.",
-        },
-        {
-            type: "input",
-            name: "Installation",
-            message: "how do you install your project?",
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "how is your project used?",
-        },
-        {
-            type: "checkbox",
-            name: "license",
-            message: "which of these license is applicable to your repo?",
-            choices: ["APACHE", "TOMCAST", "MIDDLEWARE","MIT","NONE"]
-        },
-        {
-            type: "input",
-            name: "contributor",
-            message: "please list contributors?",
-        },
-        {
-            type: "input",
-            name: "test",
-            message: "require test? please provide a walkthrough for tests.",
-        },
-        {
-            type:"input",
-            name:"github",
-            message:"what is your github user name?",
-        },
-        {
-            type:"input",
-            name:"email",
-            message:"what is your email address?",
-        },
-    ]);
+console.log(title,
+  description,
+  installation,
+  usage,
+  license,
+  contributor,
+  test,
+  github,
+  email,)
