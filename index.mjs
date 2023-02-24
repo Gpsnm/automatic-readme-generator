67885e07a10c6fbddb345dd2c4e9a0d6e3ff2e4a
adam willetts <adam-willetts@hotmail.co.uk>
2023-02-24 18:55:11 +0000

    added questions for users

--- index.mjs
+++ index.mjs
@@ -1,2 +1,53 @@
 import inquirer from "inquirer";
 import fs from "fs";
+
+inquirer.prompt(
+
+    [
+        {
+            type: "input",
+            name: "title",
+            message: "what is the title of the project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "please write a brief description.",
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: "how do you install your project?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "how is your project used?",
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "which of these license is applicable to your repo?",
+            choices: ["APACHE", "TOMCAST", "MIDDLEWARE","MIT","NONE"]
+        },
+        {
+            type: "input",
+            name: "contributor",
+            message: "please list contributors?",
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "require test? please provide a walkthrough for tests.",
+        },
+        {
+            type:"input",
+            name:"github",
+            message:"what is your github user name?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"what is your email address?",
+        },
+    ]);
